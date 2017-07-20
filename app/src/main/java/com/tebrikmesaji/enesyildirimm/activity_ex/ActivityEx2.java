c9a4da82bd8ce33fb65f61e9b11d1fabac755f82
benenesyildirim <benenesyildirim@gmail.com>
2017-07-20 14:31:42 +0300

    Initial commit

---
+++ app/src/main/java/com/tebrikmesaji/enesyildirimm/activity_ex/ActivityEx2.java
@@ -0,0 +1,13 @@
+package com.tebrikmesaji.enesyildirimm.activity_ex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEx2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex2);
+    }
+}
